311c87b3380f1a0f0ecbbd6b321919c7b25c6624
Maxine <maxinemeurer@yahoo.com>
2021-02-21 16:44:12 -0500

    commit tree d366ca8b5c8800513856ce5d82af2751fb4eb413

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :length1, :length2, :length3
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    real_triangle
+    if length1 == length2 && length2 == length3
+      return :equilateral
+    elsif length1 == length2 || length2 == length3 || length1 == length3
+      return :isosceles
+    else length1 != length2 && length2 != length3
+      return :scalene
+    end
+  end
+
+  def real_triangle
+    lengths = [length1, length2, length3]
+    equalities = [(length1 + length2 > length3), (length2 + length3 > length1), (length1 + length3 > length2)]
+    lengths.each do |side|
+      equalities << false if side <= 0
+    raise TriangleError if equalities.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "Triangle Error!"
+  end
 end
